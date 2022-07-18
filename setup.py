71eae4a98a71ff9640d6d5ca32bb88e052158318
Stefanos Gregory <gregorystefanos@gmail.com>
2022-07-19 00:08:24 +0300

    repair structure

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup, find_packages
+import setuptools
+from setuptools import setup
 
 
 setup(
     name='pythonConsoleConfigs',
-    version='0.9',
+    version='0.9.3',
     license='Apache License',
     author="Stefanos Grigori",
     author_email='gregorystefanos@gmail.com',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=setuptools.find_packages(),
     url='https://github.com/StefanosGregory/PythonConsoleConfig',
     keywords='Python Console Configuration'
 )
--- src/pythonConsoleConfigs/Font/__init__.py
+++
@@ -1,3 +0,0 @@
-from src.pythonConsoleConfigs.Font.Colors import Color
-from src.pythonConsoleConfigs.Font.Highlights import Highlight
-from src.pythonConsoleConfigs.Font.Styles import Style
